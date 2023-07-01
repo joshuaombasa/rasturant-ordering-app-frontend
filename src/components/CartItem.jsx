aff68fb3affb9a50e2288fd44cc1e30d349049e0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-01 18:11:14 +0300

    added cartItem jsx

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,16 @@
+import React, {useContext} from "react";
+import { Menucontext } from "../Menucontext";
+export default function CartItem({item}) {
+
+    const {removeItemFromCart} = useContext(Menucontext)
+    return (
+        <div className="cart--item--container">
+            <h1 className="image">{item.emoji}</h1>
+            <div className="foodItem--details">
+                <h3 className="food--name">{item.name}</h3>
+                <p className="food--price">$:{item.price}</p>
+            </div>
+            <p className="remove" onClick={() => removeItemFromCart(item.id)}>Remove</p>
+        </div>
+    )
+}
